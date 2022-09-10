b544a5bb264fdd7caace46cbbcf8a67f5da15e94
Arie <91861324+ariear@users.noreply.github.com>
2022-09-10 15:57:21 +0700

    Add files via upload

--- pendaftaran.sql
+++ pendaftaran.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Sep 10, 2022 at 02:38 AM
--- Server version: 5.7.33
--- PHP Version: 8.1.2
+-- Host: localhost
+-- Generation Time: Sep 10, 2022 at 10:56 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -50,9 +50,10 @@ CREATE TABLE `calon_siswa` (
 
 INSERT INTO `calon_siswa` (`id`, `nik`, `kk`, `nisn`, `nama`, `tgl_lahir`, `jurusan`, `tgl_pendaftaran`, `no_hp`, `alamat`, `nama_ayah`, `nama_ibu`, `asal_sekolah`, `status`) VALUES
 (3, '213213213213', '213213213213', '21321', 'arieX', '2022-09-06', 'TITL', '2022-09-07', 81221122, 'wuluhan', 'agoess pcl', 'sutresss', 'smp 1 ringintelu', 1),
-(4, '2132132132', '2312321321', '16623312', 'bambang yoga', '2022-09-07', 'RPL', '2022-09-14', 853203712, 'jember', 'sugen', 'bambang', 'smp it', 0),
+(4, '2132132132', '2312321321', '16623312', 'bambang yoga saputra', '2022-09-07', 'RPL', '2022-09-14', 853203712, 'jember', 'sugen', 'bambang', 'smp it', 1),
 (5, '3213123', '31231', '321312', 'Bang faktur', '2022-09-09', 'RPL', '2022-09-16', 432424242, 'fsfdsfdsfds', 'Wahyu', 'sutres', '', 0),
-(6, '92378971237', '2371237', '92139127371273', 'dioasidoias', '2022-09-14', 'RPL', '2022-09-16', 823718231, 'jember\r\npuger', 'sutreees2', 'sutress3', 'smppp 5', 1);
+(6, '92378971237', '2371237', '92139127371273', 'dioasidoias', '2022-09-14', 'RPL', '2022-09-16', 823718231, 'jember\r\npuger', 'sutreees2', 'sutress3', 'smppp 5', 1),
+(8, '3424324', '78787', '78787878', 'fdgdfgdgdg', '2022-09-23', 'BKP', '2022-09-19', 5555, 'jkjjkg', 'dsaddsa', 'jkjkjk', 'jkjkjkjkjk', 1);
 
 -- --------------------------------------------------------
 
@@ -72,8 +73,7 @@ INSERT INTO `jurusan` (`jurusan`) VALUES
 ('BKP'),
 ('RPL'),
 ('TAB'),
-('TITL'),
-('TKR');
+('TITL');
 
 -- --------------------------------------------------------
 
@@ -127,7 +127,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `calon_siswa`
 --
 ALTER TABLE `calon_siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -143,7 +143,7 @@ ALTER TABLE `user`
 -- Constraints for table `calon_siswa`
 --
 ALTER TABLE `calon_siswa`
-  ADD CONSTRAINT `calon_siswa_ibfk_1` FOREIGN KEY (`jurusan`) REFERENCES `jurusan` (`jurusan`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `calon_siswa_ibfk_1` FOREIGN KEY (`jurusan`) REFERENCES `jurusan` (`jurusan`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
