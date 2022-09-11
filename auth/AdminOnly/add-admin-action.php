4fe177b28b1545795d4608fdd766145cdb6c602c
dio-nugraha <56766061+dio-nugraha@users.noreply.github.com>
2022-09-11 15:47:38 +0700

    For Add admin :)

---
+++ auth/AdminOnly/add-admin-action.php
@@ -0,0 +1,11 @@
+<?php
+    require('../../koneksi.php');
+
+        $email = $_POST['email'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        $query = mysqli_query($conn, "insert into user(`id`, `name`, `email`, `no_telp`, `photo_profile`, `password`) VALUES (NULL,'','$email', '', '','$password')");
+        
+        header('Location: ../login.php');
+        
+?>
